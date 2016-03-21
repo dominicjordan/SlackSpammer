95a927a73b5c7b4c52a59f12933e58d6b5b0416b
dominicjordan <dominic007@live.nl>
2016-03-21 12:19:08 +0100

    Update spammer.js
    Added a function to drop all users with a certain amount with the @plusplus plugin.

--- spammer.js
+++ spammer.js
@@ -12,3 +12,21 @@ var interval;
  function stopSpam(){
      clearInterval(interval);
  }
+ 
+ function getAllUsernames() {
+    var names = [];
+    var members = parseInt(document.getElementById("member_count_title").getElementsByTagName("span")[0].innerHTML.replace('/', ''));
+    for (var index = 1; index < members; index++) {
+        var name = document.getElementsByClassName("channel_page_member_row")[index].getElementsByTagName("a")[1].innerHTML;
+        names.push(name);
+    }
+    return names;
+}
+
+function dropAllToLow(number) {
+    var users = getAllUsernames();
+    users.forEach(function(e) {
+        var msg = "@"+e+"--";
+        spam(msg, number);
+    });
+}
